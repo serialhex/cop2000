980f306a74b7c4ba88cbfecacab6c9c9dc00b01c
Justin Patera <serialhex@gmail.com>
2011-09-15 15:00:08 -0400

    updated assignement 2 for the 3rd time

--- assignment_2/a2.cpp
+++ assignment_2/a2.cpp
@@ -11,7 +11,7 @@
 
 #include <iostream>
 
-// 
+// constant
 #define FACTOR 2.54
 
 using namespace std;
@@ -24,17 +24,32 @@ int main() {
 	int inches, feet;
 
 	// print intro message
-	cout << "\
-\tHeight Converting Program\n\
-\tDesigned by Randolph Gibson - 1 August 2011\n\
-\tCoded by Justin Patera - 2011-09-13\n\
+	/*  may be causing errors on windows
+	cout << "\n\
+Height Converting Program\n\
+Designed by Randolph Gibson - 1 August 2011\n\
+Coded by Justin Patera - 2011-09-13\n\
 \n\
-\tThis program will convert a person's height from meters\n\
-\tinto feet and inches (rounded to the nearest inch) and\n\
-\tdisplay the results on the screen. The height must be\n\
-\tentered in metric units and may contain decimal portions.\n\
-\tThe answer will be displayed in whole feet and whole inches.\n\
+This program will convert a person's height from meters\n\
+into feet and inches (rounded to the nearest inch) and\n\
+display the results on the screen. The height must be\n\
+entered in metric units and may contain decimal portions.\n\
+The answer will be displayed in whole feet and whole inches.\n\
 \n";
+*/
+
+	// lots of cout's.. very annoying!
+	cout << endl;
+	cout << "Height Converting Program" << endl;
+	cout << "Designed by Randolph Gibson - 1 August 2011" << endl;
+	cout << "Coded by Justin Patera - 2011-09-13" << endl;
+	cout << endl;
+	cout << "This program will convert a person's height from meters" << endl;
+	cout << "into feet and inches (rounded to the nearest inch) and" << endl;
+	cout << "display the results on the screen. The height must be" << endl;
+	cout << "entered in metric units and may contain decimal portions." << endl;
+	cout << "The answer will be displayed in whole feet and whole inches." << endl;
+	cout << endl;
 
 	// the Q
 	cout << "Enter the person's height in meters: ";
@@ -45,7 +60,7 @@ int main() {
 	feet = inches / 12;
 	inches %= 12;
 
-	cout << "\nThat height is equivalent to " << feet << " feet and " << inches << " inch(es).\n";
+	cout << "\nThat height is equivalent to " << feet << " feet and " << inches << " inch(es).\n" << endl;
 
 	return 0;
 }
