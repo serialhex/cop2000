7b7041dba1353c03a6b3f9122a5a8727ca3c9660
Justin Patera <serialhex@gmail.com>
2011-09-15 16:52:10 -0400

    final update for assignment 2

--- assignment_2/a2.cpp
+++ assignment_2/a2.cpp
@@ -20,31 +20,16 @@ using namespace std;
 int main() {
 
 	// declare variables
-	float meter_h;
-	int inches, feet;
+	float HM;
+	int INCHES, FEET;
 
 	// print intro message
-	/*  may be causing errors on windows
-	cout << "\n\
-Height Converting Program\n\
-Designed by Randolph Gibson - 1 August 2011\n\
-Coded by Justin Patera - 2011-09-13\n\
-\n\
-This program will convert a person's height from meters\n\
-into feet and inches (rounded to the nearest inch) and\n\
-display the results on the screen. The height must be\n\
-entered in metric units and may contain decimal portions.\n\
-The answer will be displayed in whole feet and whole inches.\n\
-\n";
-*/
-
-	// lots of cout's.. very annoying!
 	cout << endl;
 	cout << "Height Converting Program" << endl;
 	cout << "Designed by Randolph Gibson - 1 August 2011" << endl;
 	cout << "Coded by Justin Patera - 2011-09-13" << endl;
 	cout << endl;
-	cout << "This program will convert a person's height from meters" << endl;
+	cout << "This program will convert a person\'s height from meters" << endl;
 	cout << "into feet and inches (rounded to the nearest inch) and" << endl;
 	cout << "display the results on the screen. The height must be" << endl;
 	cout << "entered in metric units and may contain decimal portions." << endl;
@@ -52,15 +37,15 @@ The answer will be displayed in whole feet and whole inches.\n\
 	cout << endl;
 
 	// the Q
-	cout << "Enter the person's height in meters: ";
-	cin >> meter_h;
+	cout << "Enter the person\'s height in meters: ";
+	cin >> HM;
 
 	// the fun maths
-	inches = static_cast<int>(meter_h * 100 / FACTOR);
-	feet = inches / 12;
-	inches %= 12;
+	INCHES = static_cast<int>((HM * 100 / FACTOR)+0.5);
+	FEET = INCHES / 12;
+	INCHES %= 12;
 
-	cout << "\nThat height is equivalent to " << feet << " feet and " << inches << " inch(es).\n" << endl;
+	cout << "\nThat height is equivalent to " << FEET << " feet and " << INCHES << " inch(es).\n" << endl;
 
 	return 0;
 }
